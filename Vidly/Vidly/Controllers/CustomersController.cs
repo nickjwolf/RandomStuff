1ee5bbbc95d01c2003156eb4d1e24d04e08952b0
nickjwolf <nickjwolf@gmx.de>
2022-03-22 21:22:23 +0100

    Validation for Customer Input Fields

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -54,6 +54,17 @@ namespace Vidly.Controllers
         // public ActionResult Create(CustomerFormViewModel viewModel)
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
