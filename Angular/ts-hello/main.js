3dc272545ff85434b55bae91d6999a8acbadcad3
nickjwolf <nickjwolf@gmx.de>
2022-03-29 17:56:54 +0200

    Chapter 14 done

--- Angular/ts-hello/main.js
+++ Angular/ts-hello/main.js
@@ -1,14 +1,90 @@
-function log(message) {
-    console.log(message);
-}
-var message = 'Hello World';
-log(message);
-var number = 1;
-var count = 2;
-doSomething();
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
+var Point = /** @class */ (function () {
+    //  private y: number;
+    //  private x: number;
+    //  constructor(x?: number, y?: number) {
+    //      this.x = x;
+    //      this.y = y;
+    //  }
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+        // creates class variables x and y and uses this.x = x
     }
-    console.log('Finally: ' + i);
-}
+    Point.prototype.draw = function () {
+        console.log('X ' + this._x + ', Y: ' + this._y);
+    };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('value can not be less then 0');
+            else {
+                this._x = value;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Point;
+}());
+var point = new Point(1, 2);
+var x = point.x();
+point.draw();
+// interface Point { // Interfaces in Pascal Naming convetions --> Upper starting letter
+//     x: number,
+//     y: number
+//     draw: () => void
+// }
+// let drawPoint = (point: Point) => {
+//     //...
+// }
+// let getDistance = (point A: Point, pointB : Point){
+//     // :...
+// }
+// drawPoint({
+//     x: 1, y:2
+// })
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'a', false];
+// const colorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2; 
+// enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
+// let BackgroundColor = Color.Red;
+// //let message = 'abc';
+//  let message;
+//  message = 'abc';
+// //var endwithc = (<string>message).endsWith('c');
+// var endwithc = (message as string).endsWith('c');
+// let count = 5;
+// count = 'a';
+// function log(message) {
+//     console.log(message);
+// }
+// var message = 'Hello World';
+// log(message);
+// var number = 1;
+// let count = 2;
+// doSomething();
+// // dosen't work
+// function doSomething(){
+//     for (let i = 0; i < 5; i++)
+//     {
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// works 
+// function doSomething(){
+//     for (var i = 0; i < 5; i++)
+//     {
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
--- Angular/ts-hello/main.ts
+++ Angular/ts-hello/main.ts
@@ -1,12 +1,114 @@
-function log(message) {
-    console.log(message);
+
+
+class Point { 
+     
+    //  private y: number;
+    //  private x: number;
+    
+    //  constructor(x?: number, y?: number) {
+    //      this.x = x;
+    //      this.y = y;
+    //  }
+
+    constructor(private _x?: number, private _y?: number) {
+        // creates class variables x and y and uses this.x = x
+    }
+
+
+    public draw() {
+        console.log('X '+  this._x + ', Y: ' + this._y);
+    }
+    
+
+    get x(){
+        return this._x;
+    }
+
+    set x(value) {
+        if(value < 0)
+            throw new Error('value can not be less then 0')
+        else{
+                this._x = value;
+            }
+    }
+
 }
 
-var message = 'Hello World';
-log(message);
 
-var number = 1;
-let count = 2;
+let point  = new Point(1,2);
+let x = point.x;
+point.draw();
+
+
+
+// interface Point { // Interfaces in Pascal Naming convetions --> Upper starting letter
+//     x: number,
+//     y: number
+//     draw: () => void
+
+// }
+
+
+// let drawPoint = (point: Point) => {
+//     //...
+// }
+
+
+// let getDistance = (point A: Point, pointB : Point){
+//     // :...
+// }
+
+
+
+// drawPoint({
+//     x: 1, y:2
+// })
+
+
+
+
+
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'a', false];
+
+
+// const colorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2; 
+
+// enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
+// let BackgroundColor = Color.Red;
+
+// //let message = 'abc';
+//  let message;
+//  message = 'abc';
+
+// //var endwithc = (<string>message).endsWith('c');
+// var endwithc = (message as string).endsWith('c');
+
+
+
+
+
+// let count = 5;
+// count = 'a';
+
+
+
+
+// function log(message) {
+//     console.log(message);
+// }
+
+// var message = 'Hello World';
+// log(message);
+
+// var number = 1;
+// let count = 2;
 
 // doSomething();
 
